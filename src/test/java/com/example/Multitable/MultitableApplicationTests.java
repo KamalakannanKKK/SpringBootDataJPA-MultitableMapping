49151125f5b8ec893260cf181301c36b14bdbfaa
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-04-28 14:20:23 +0530

    Create MultitableApplicationTests.java

---
+++ src/test/java/com/example/Multitable/MultitableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Multitable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
